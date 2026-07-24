705a4543d66ac4f30ee1bc8645696e273e112ba3
Yusef <mulaev2006@gmail.com>
2026-07-24 22:03:54 +0400

    feat(user-service): add user api dto contracts

--- backend/user-service/src/main/java/com/touroperator/user_service/application/dto/response/AuthResponse.java
+++ backend/user-service/src/main/java/com/touroperator/user_service/application/dto/response/AuthResponse.java
@@ -1,4 +1,14 @@
 package com.touroperator.user_service.application.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class AuthResponse {
+
+    private Long userId;
+    private String email;
+    private String role;
+    private String accessToken;
 }
--- backend/user-service/src/main/java/com/touroperator/user_service/application/dto/response/UserResponse.java
+++ backend/user-service/src/main/java/com/touroperator/user_service/application/dto/response/UserResponse.java
@@ -1,4 +1,13 @@
 package com.touroperator.user_service.application.dto.response;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
+@AllArgsConstructor
 public class UserResponse {
+
+    private Long id;
+    private String email;
+    private String role;
+    private boolean active;
 }
