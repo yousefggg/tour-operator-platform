c773906d49eedb49a8e88243413e0c25508c23d9
Yusef <mulaev2006@gmail.com>
2026-07-24 22:03:07 +0400

    feat(user-service): add user persistence entity

--- backend/user-service/src/main/java/com/touroperator/user_service/infrastructure/adapter/out/persistence/entity/UserEntity.java
+++ backend/user-service/src/main/java/com/touroperator/user_service/infrastructure/adapter/out/persistence/entity/UserEntity.java
@@ -1,4 +1,40 @@
 package com.touroperator.user_service.infrastructure.adapter.out.persistence.entity;
 
+import com.touroperator.user_service.domain.model.Role;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
 public class UserEntity {
+
+    @Id
+    @SequenceGenerator(
+            name = "user_seq",
+            sequenceName = "user_seq",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "user_seq"
+    )
+    private Long id;
+
+    @Column(unique = true, nullable = false, length = 255)
+    private String email;
+
+    @Column(nullable = false, length = 255)
+    private String passwordHash;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Column(nullable = false)
+    private boolean active;
 }
