e21afa19098ec0dbefa7fd28dfc43415d4ac8d96
Yusef <mulaev2006@gmail.com>
2026-07-24 22:01:48 +0400

    feat(user-service): add user already exists exception

--- backend/user-service/src/main/java/com/touroperator/user_service/domain/exception/UserAlreadyExistsException.java
+++ backend/user-service/src/main/java/com/touroperator/user_service/domain/exception/UserAlreadyExistsException.java
@@ -1,7 +1,7 @@
 package com.touroperator.user_service.domain.exception;
 
-public class UserAlreadyExistsException extends RuntimeException {
-  public UserAlreadyExistsException(String message) {
-    super(message);
-  }
+public class UserAlreadyExistsException extends DomainException {
+    public UserAlreadyExistsException(String email) {
+        super("User already exists with email: " + email);
+    }
 }
