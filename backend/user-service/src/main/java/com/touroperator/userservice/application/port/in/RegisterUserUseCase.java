36f49d11fdcaae41e103bde59315a90c4614a386
Yusef <mulaev2006@gmail.com>
2026-08-08 11:29:54 +0400

    feat(application): add register user use case

--- backend/user-service/src/main/java/com/touroperator/userservice/application/port/in/RegisterUserUseCase.java
+++ backend/user-service/src/main/java/com/touroperator/userservice/application/port/in/RegisterUserUseCase.java
@@ -1,4 +1,9 @@
 package com.touroperator.userservice.application.port.in;
 
+import com.touroperator.userservice.application.command.RegisterUserCommand;
+import com.touroperator.userservice.application.dto.UserResponse;
+
 public interface RegisterUserUseCase {
+
+    UserResponse register(RegisterUserCommand command);
 }
