ce4a787e929aadd3f68d01d8b0afab8015be1660
Yusef <mulaev2006@gmail.com>
2026-08-08 11:30:14 +0400

    feat(application): add update user use case

--- backend/user-service/src/main/java/com/touroperator/userservice/application/port/in/UpdateUserUseCase.java
+++ backend/user-service/src/main/java/com/touroperator/userservice/application/port/in/UpdateUserUseCase.java
@@ -1,4 +1,9 @@
 package com.touroperator.userservice.application.port.in;
 
+import com.touroperator.userservice.application.command.UpdateUserCommand;
+import com.touroperator.userservice.application.dto.UserResponse;
+
 public interface UpdateUserUseCase {
+
+    UserResponse update(Long id, UpdateUserCommand command);
 }
