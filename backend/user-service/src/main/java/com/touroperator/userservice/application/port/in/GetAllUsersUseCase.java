030024c99f289513d33aaa9e67b0ee6f4c99b30f
Yusef <mulaev2006@gmail.com>
2026-08-08 11:30:08 +0400

    feat(application): add get all users use case

--- backend/user-service/src/main/java/com/touroperator/userservice/application/port/in/GetAllUsersUseCase.java
+++ backend/user-service/src/main/java/com/touroperator/userservice/application/port/in/GetAllUsersUseCase.java
@@ -1,4 +1,10 @@
 package com.touroperator.userservice.application.port.in;
 
+import com.touroperator.userservice.application.dto.UserResponse;
+
+import java.util.List;
+
 public interface GetAllUsersUseCase {
+
+    List<UserResponse> getAll();
 }
